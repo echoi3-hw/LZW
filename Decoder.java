9d2b54a0308be435954c6b4156b489c52a242852
LWeet7 <liamweetman@gmail.com>
2020-09-15 11:16:48 -0700

    Rough Draft of Early Stage

--- Decoder.java
+++ Decoder.java
@@ -1,4 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Decoder {
-
+	public Decoder () {
+	}
+	
+	public void decode (String fileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+		PrintWriter pw = new PrintWriter (new FileWriter (fileName.substring(0, fileName.length()-4)+".txt"));
+		ArrayList<String> key = new ArrayList<String> ();
+		for (int i = 0; i < 256; i++) {
+			key.add(""+(char)i);
+		}
+		String updatedWord = "";
+		//I am able to get the number correctly
+		while (br.ready()) {
+			int codedLetter = br.read();
+			updatedWord += (char)codedLetter;
+			if (key.size() > codedLetter) {
+				pw.print(key.get(codedLetter));
+			}
+		}
+	}
 }
