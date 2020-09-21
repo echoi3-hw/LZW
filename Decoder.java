633129b051b8730ec4a1cbff40b20a1dcbfec153
amaanirfan <amaanirf@gmail.com>
2020-09-21 14:07:02 -0700

    Finished working copy!
    Realized our bug was not adding correctly.
    
    Also added comments to our decoder here!

--- Decoder.java
+++ Decoder.java
@@ -10,45 +10,43 @@ import java.util.HashMap;
 
 public class Decoder {
 	public Decoder () {
-		}
+	}
 	
 	public void decode (String fileName) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
 		PrintWriter pw = new PrintWriter (new FileWriter (fileName.substring(0, fileName.length()-4)+"1.txt"));
 		ArrayList<String> key = new ArrayList<String> ();
+		
+		//setting up my key with ascii characters 0-255
 		for (int i = 0; i < 256; i++) {
 			key.add(""+(char)i);
 		}
-		String numberToWord = "";
-		String addedFirstWord = "";
-		String addedSecondWord = "";
-		int codedIndex;
-		//I am able to get the number correctly
-		while (br.ready()) {
-			//System.out.println(br.read());
+		
+		//declaring variables I will be using
+		String numberToWord = ""; //this is the output that will be printed
+		String addedFirstWord = ""; //the last thing that was just printed, used to add to our key with the next character (builds on itself)
+		String addedSecondWord = ""; //new string being read in up until we don't already have it in our key
+		int codedIndex; //this is what is read in by our buffered reader
+		
+		while (br.ready()) { //will continue intil the last character of the .lzw file is read in
 			codedIndex = br.read();
-			//System.out.println (codedIndex);
-			if (key.size() > codedIndex) {
-				numberToWord = key.get(codedIndex);
-				
+			
+			if (key.size() > codedIndex) { //this is the "typical case" in which the character we just read in (codedIndex) already exists within our key
+				numberToWord = key.get(codedIndex); //getting the string associated with that character in our key
+				pw.print(numberToWord); 
+			}
+			else { //this is the "special case" in which the character we read in (codedIndex) is not already in the key
+				numberToWord = addedFirstWord + addedFirstWord.substring(0,1); //as discussed in class, we can simply use the last word + its first letter. ex: liam is the last word --> liaml
+				key.add(numberToWord); //this is added to our key for storage
 				pw.print(numberToWord);
 			}
-			else {
-				key.add(addedFirstWord + addedFirstWord.substring(0,1));
-				numberToWord = key.get(codedIndex);
-				System.out.println("ELSE TRIGGERED");
-				pw.print(numberToWord);
+			
+			addedSecondWord = numberToWord; //we store what was just printed
+			
+			if (!addedFirstWord.equals("")) { //this is simply to make sure that this isnt the very start of the .lzw file, in which we don't have anything to add to our key. (A more efficient way to do this may be with a counter, but oh well...)
+				key.add(addedFirstWord + addedSecondWord.substring(0, 1)); //add to our key here
 			}
-			//System.out.println (numberToWord);
-			//System.out.println("c");
-			addedSecondWord = numberToWord;
-			//System.out.println(addedFirstWord);
-			//System.out.println(addedSecondWord);
-			if (!addedFirstWord.equals("") && !key.contains(addedFirstWord + addedSecondWord)) {
-				key.add(addedFirstWord + addedSecondWord);
-			}
-			addedFirstWord = addedSecondWord;
-			//addedSecondWord = "";
+			addedFirstWord = addedSecondWord; //what was just printed becomes stored for the next iteration of the while loop
 		}
 		br.close();
 		pw.close();
