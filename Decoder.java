c68a2d2e0884c5d3e61c6b0e04dbfeeb86561e44
LWeet7 <liamweetman@gmail.com>
2020-09-20 18:12:28 -0700

    Updated decoded

--- Decoder.java
+++ Decoder.java
@@ -14,7 +14,7 @@ public class Decoder {
 	
 	public void decode (String fileName) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
-		PrintWriter pw = new PrintWriter (new FileWriter (fileName.substring(0, fileName.length()-4)+".txt"));
+		PrintWriter pw = new PrintWriter (new FileWriter (fileName.substring(0, fileName.length()-4)+"1.txt"));
 		ArrayList<String> key = new ArrayList<String> ();
 		for (int i = 0; i < 256; i++) {
 			key.add(""+(char)i);
@@ -24,16 +24,19 @@ public class Decoder {
 		while (br.ready()) {
 			String addedFirstWord = "";
 			String addedSecondWord = "";
-			int codedLetter = br.read();
-			if (key.size() < codedLetter) {
-				numberToWord += key.get(key.indexOf(codedLetter));
+			//System.out.println(br.read());
+			int codedIndex = br.read();
+			System.out.println (codedIndex);
+			if (key.size() > codedIndex) {
+				numberToWord += key.get(key.indexOf(codedIndex));
 				pw.print(numberToWord);
 			}
 			else {
 				key.add(addedFirstWord + addedFirstWord.substring(0,1));
-				numberToWord += key.get(key.indexOf(codedLetter));
+				numberToWord += key.get(key.indexOf(codedIndex));
 				pw.print(numberToWord);
 			}
+			System.out.println("c");
 			addedSecondWord = numberToWord;
 			if (!addedFirstWord.equals("")) {
 				key.add(addedFirstWord + addedSecondWord);
