91be8b8fa5c36bbf7dddbd2d72936b984b4daf14
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-27 16:43:33 -0700

    Update Decoder.java

--- Decoder.java
+++ Decoder.java
@@ -5,12 +5,21 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Decoder {
 	private final int ORIG_LENGTH = 256; //starting length of the arraylist
 	private int maxLength;
+	
+	/*
+	 * Edit 1
+	 */
+	private final int MAXSIZE = 4096; //not sure, just looked this up??
+	ArrayDeque<Integer> recentValues = new ArrayDeque<Integer>();
+	int [] codefreqs = new int [MAXSIZE];
+	
 	public Decoder (int maxLength) {
 		this.maxLength = maxLength;
 	}
@@ -18,11 +27,12 @@ public class Decoder {
 	public void decode (String fileName) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
 		PrintWriter pw = new PrintWriter (new FileWriter (fileName.substring(0, fileName.length()-4)+"1.txt"));
-		ArrayList<String> key = new ArrayList<String> ();
+		//ArrayList<String> key = new ArrayList<String> ();
+		HashMap<Integer, String> dict = new HashMap<Integer, String>(); // EDIT 3: THEY USED AN ARRAYLIST NOT A HASHMAP
 		
 		//setting up my key with ascii characters 0-255
 		for (int i = 0; i < ORIG_LENGTH; i++) {
-			key.add(""+(char)i);
+			key.put(i, ""+(char)i);
 		}
 		
 		//declaring variables I will be using
@@ -34,20 +44,25 @@ public class Decoder {
 		while (br.ready()) { //will continue intil the last character of the .lzw file is read in
 			codedIndex = br.read();
 			
-			if (key.size() > codedIndex) { //this is the "typical case" in which the character we just read in (codedIndex) already exists within our key
+			if (dict.containsKey(codedIndex)) { //this is the "typical case" in which the character we just read in (codedIndex) already exists within our key
 				numberToWord = key.get(codedIndex); //getting the string associated with that character in our key
-				pw.print(numberToWord); 
+				pw.print(numberToWord);
+				
+				recentValues.add(codedIndex); // EDIT 2
+				codefreqs[codedIndex]++;
 			}
 			else { //this is the "special case" in which the character we read in (codedIndex) is not already in the key
 				numberToWord = addedFirstWord + addedFirstWord.substring(0,1); //as discussed in class, we can simply use the last word + its first letter. ex: liam is the last word --> liaml
-				key.add(numberToWord); //this is added to our key for storage
+				key.put(table.size(), numberToWord); //this is added to our key for storage
 				pw.print(numberToWord);
+				
+				
 			}
 			
 			addedSecondWord = numberToWord; //we store what was just printed
 			
 			if (!addedFirstWord.equals("") && key.size() < maxLength) { //this is simply to make sure that this isnt the very start of the .lzw file, in which we don't have anything to add to our key. (A more efficient way to do this may be with a counter, but oh well...)
-				key.add(addedFirstWord + addedSecondWord.substring(0, 1)); //add to our key here
+				key.put(table.size(), addedFirstWord + addedSecondWord.substring(0, 1)); //add to our key here
 			}
 			addedFirstWord = addedSecondWord; //what was just printed becomes stored for the next iteration of the while loop
 			System.out.println(key.size());
