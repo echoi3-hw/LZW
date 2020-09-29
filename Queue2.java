800885bbd7069b0a37ff1c54284254ec6f9f3787
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-29 09:29:51 -0700

    Update Queue2.java

--- Queue2.java
+++ Queue2.java
@@ -1,4 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.util.PriorityQueue;
 
 public class Queue2 {
+	PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+	
+	public void pushToBack ()
+	{
+		
+	}
 
 }
