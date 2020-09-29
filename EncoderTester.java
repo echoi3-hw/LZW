50000097233d75ad48f8c923eb031ec6528eec66
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-29 09:10:47 -0700

    Update EncoderTester.java

--- EncoderTester.java
+++ EncoderTester.java
@@ -1,11 +1,10 @@
 import java.io.IOException;
 
+
 public class Queue
+
 {
 	
-	public static void main (String [] args) throws IOException
-	{
-		Encoder eli = new Encoder (4097);
-		eli.encode("message.txt");
-	}
+	
+	
 }
